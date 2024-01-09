d9ae6c9fb07a241408a15ccb190e84ec28247ac2
lynn <latt3770@gmail.com>
2024-01-09 10:39:41 +0630

    data insert

--- src/main/java/com/example/bookshop/BookShopApplication.java
+++ src/main/java/com/example/bookshop/BookShopApplication.java
@@ -14,6 +14,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 @RequiredArgsConstructor
@@ -23,11 +25,13 @@ public class BookShopApplication {
     private final GenreDao genreDao;
     private final PublisherDao publisherDao;
 
+
     @Bean
-    public ApplicationRunner runner(){
+    @Transactional @Profile("data")
+    public ApplicationRunner runner() {
         return r -> {
-            Author author = new Author("Charles Dickens","charles@gmail.com");
-            Author author1 = new Author("Thomas Hardy","thomas@gmail.com");
+            Author author = new Author("Charles Dickens", "charles@gmail.com");
+            Author author1 = new Author("Thomas Hardy", "thomas@gmail.com");
 
             Book book = new Book(
                     1, IsbnGenerator.generate(),
@@ -38,17 +42,104 @@ public class BookShopApplication {
                     "https://source.unplash.com/400x300/?flower"
 
             );
-            Book book1 = new Book();
-            Book book2 = new Book();
-            Book book3 = new Book();
+            Book book1 = new Book(
+                    2, IsbnGenerator.generate(),
+                    "Great Expectation",
+                    "GoodChoice",
+                    30.5,
+                    20,
+                    "https://source.unplash.com/400x300/?nature"
+
+            );
+            Book book2 = new Book(
+                    3, IsbnGenerator.generate(),
+                    "Bleak House",
+                    "Nice",
+                    40.0,
+                    20,
+                    "https://source.unplash.com/400x300/?ocean"
+
+            );
+            Book book3 = new Book(
+                    4, IsbnGenerator.generate(),
+                    "Under The Greenwood Tree",
+                    "Excellent",
+                    80.0,
+                    20,
+                    "https://source.unplash.com/400x300/?flower"
+
+            );
+            Book book4 = new Book(
+                    1, IsbnGenerator.generate(),
+                    "Return Of  The Native",
+                    "Nice",
+                    70.0,
+                    20,
+                    "https://source.unplash.com/400x300/?flower"
+
+            );
+            Book book5 = new Book(
+                    1, IsbnGenerator.generate(),
+                    "Far From the Maddening Crowd",
+                    "Wow,Your Choice was good",
+                    100.0,
+                    20,
+                    "https://source.unplash.com/400x300/?flower"
+
+            );
 
             Genre genre = new Genre();
             genre.setGenreName("Tragedy");
+
             Genre genre1 = new Genre();
             genre1.setGenreName("Adventure");
 
-            Publisher publisher = new Publisher("New Era","new@gmail.com");
-            Publisher publisher1 = new Publisher("","");
+            Publisher publisher = new Publisher("New Era", "new@gmail.com");
+            Publisher publisher1 = new Publisher("", "");
+
+
+
+            //mapping
+            author.addBook(book);
+            author.addBook(book1);
+            author.addBook(book2);
+
+            author1.addBook(book3);
+            author1.addBook(book4);
+            author1.addBook(book5);
+
+            Publisher pub1 = publisherDao.save(publisher);
+            pub1.addBook(book);
+            pub1.addBook(book1);
+            pub1.addBook(book2);
+
+            Publisher pub2  = publisherDao.save(publisher1);
+            pub2.addBook(book3);
+            pub2.addBook(book4);
+            pub2.addBook(book5);
+
+            Genre gen1 = genreDao.save(genre);
+            book.addGenres(gen1);
+            book1.addGenres(gen1);
+            book2.addGenres(gen1);
+
+            Genre gen2 = genreDao.save(genre1);
+            book3.addGenres(gen2);
+            book4.addGenres(gen2);
+            book5.addGenres(gen2);
+
+
+//            bookDao.save(book);
+//            bookDao.save(book1);
+//            bookDao.save(book2);
+//            bookDao.save(book3);
+//            bookDao.save(book4);
+//            bookDao.save(book5);
+
+            authorDao.save(author);
+            authorDao.save(author1);
+
+
 
         };
     }
